6c4dd7b115072db023e6515d2751647c30683356
Zacaria <zchtatar@excilys.com>
2017-01-11 10:43:49 +0100

    [UP] : logs

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const fs = require(`fs`);
 const path = require(`path`);
-console.log(__dirname);
+
+console.log('Running from root directory : node index.js. ', __dirname);
 
 // Success with node index.js
 fs.readFile(path.join(__dirname, `path/to/myFile.txt`), (err, data) => {
   if (err) {
-    console.log('dirname : failed !', err);
+    console.log('dirname : failed ', err);
   } else {
-    console.log(data);
+    console.log('dirname : success ' + data);
   }
 });
 
 // Success with node index.js
 fs.readFile(`./path/to/myFile.txt`, (err, data) => {
   if (err) {
-    console.log('relative : failed !', err.code);
+    console.log('relative : failed ', err.code);
   } else {
-    console.log(data);
+    console.log('relative : success ' + data);
   }
 });
--- path/index.js
+++ path/index.js
@@ -1,21 +1,22 @@
 const fs = require(`fs`);
 const path = require(`path`);
-console.log(__dirname);
+
+console.log('Running from root directory : node path/index.js. ', __dirname);
 
 // Fails with node path/index.js
 fs.readFile(path.join(__dirname, `to/myFile.txt`), (err, data) => {
   if (err) {
-    console.log('dirname : failed !', err.code);
+    console.log('dirname : failed ', err.code);
   } else {
-    console.log(data);
+    console.log('dirname : success ' + data);
   }
 });
 
 // Success with node path/index.js
 fs.readFile(`./to/myFile.txt`, (err, data) => {
   if (err) {
-    console.log('relative : failed !', err.code);
+    console.log('relative : failed ', err.code);
   } else {
-    console.log(data);
+    console.log('relative : success ' + data);
   }
 });
